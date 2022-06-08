d25fe5ffcb790d0b695d62295016aaaed513031e
divine <divineobi07@gmail.com>
2022-06-08 17:10:15 +0100

    fix: edit css property in jsx

--- src/components/SearchWord/SearchWord.js
+++ src/components/SearchWord/SearchWord.js
@@ -21,16 +21,16 @@ const SearchWord = () => {
               <path
                 d="M13 25C19.6274 25 25 19.6274 25 13C25 6.37258 19.6274 1 13 1C6.37258 1 1 6.37258 1 13C1 19.6274 6.37258 25 13 25Z"
                 stroke="#BEC8DA"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M22 22L29 29"
                 stroke="#BEC8DA"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
           </span>
