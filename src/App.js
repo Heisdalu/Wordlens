fd4a8678130374d1e39a8e7d32f3cb0bfe617bb7
divine <divineobi07@gmail.com>
2022-06-08 17:54:09 +0100

    add display word component and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import SearchWord from './components/SearchWord/SearchWord';
+import DisplayWord from './components/DisplayWord/DisplayWord';
 
 function App() {
   return (
     <section className="main-Container">
       <Header/>
       <SearchWord />
+      <DisplayWord />
     </section>
   );
 }
