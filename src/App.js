d6a522647609acafbe7094d70caa66303b3cf5aa
divine <divineobi07@gmail.com>
2022-06-08 21:28:17 +0100

    fix responsive issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header';
-import SearchWord from './components/SearchWord/SearchWord';
-import DisplayWord from './components/DisplayWord/DisplayWord';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Header from "./components/Header/Header";
+import SearchWord from "./components/SearchWord/SearchWord";
+import DisplayWord from "./components/DisplayWord/DisplayWord";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <section className="main-Container">
-      <Header/>
-      <SearchWord />
-      <DisplayWord />
+      <Header />
+      <section className="sub_container">
+        <SearchWord />
+        <DisplayWord />
+      </section>
       <Footer />
     </section>
   );
