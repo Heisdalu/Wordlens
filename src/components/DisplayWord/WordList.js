5d2649a7beb88acdae49ff998c00f6f911e36fc2
divine <divineobi07@gmail.com>
2022-06-09 23:28:14 +0100

    Create WordList component to field word detail and integrate into Word Content component

---
+++ src/components/DisplayWord/WordList.js
@@ -0,0 +1,24 @@
+import "./WordList.css";
+
+const WordList = (props) => {
+  const partofSpeech = props.obj.partofSpeech;
+  const synonyms = props.obj.synonyms;
+  const example = props.obj.examples;
+  const definitions = props.obj.definitions;
+
+//   console.log(partofSpeech, definitions, synonyms, example);
+  return (
+    <li className="word_content">
+      <p className="">
+          <span className='speechPart'>{`(${partofSpeech})`}</span>
+        {`${definitions}`}
+      </p>
+      <p className="word__in_sentence">{example.length > 0  ? `sentence: "${example}"` : ''}</p>
+      <p className="word_synonyms">{synonyms.length > 0 ? `Synonyms: "${synonyms}"` : ''}</p>
+    </li>
+  );
+};
+
+// sentence: "hello there, Katie";
+// Synonyms: "";
+export default WordList;
