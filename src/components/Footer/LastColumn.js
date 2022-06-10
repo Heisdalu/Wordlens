a3ce50575786523be5428f7b98eebeff4327d0b5
divine <divineobi07@gmail.com>
2022-06-10 16:19:41 +0100

    add last column component

---
+++ src/components/Footer/LastColumn.js
@@ -0,0 +1,108 @@
+import "./LastColumn.css";
+
+const LastColumn = () => {
+  return (
+    <div className="last-column">
+      <span className="third__logo">
+        <svg
+          width="170"
+          height="86"
+          viewBox="0 0 170 86"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="0.707107"
+            y="42.4264"
+            width="59"
+            height="59"
+            transform="rotate(-45 0.707107 42.4264)"
+            stroke="#616681"
+          />
+          <rect
+            x="85.4171"
+            y="42.8531"
+            width="59"
+            height="59"
+            transform="rotate(-45 85.4171 42.8531)"
+            stroke="#616681"
+          />
+          <rect
+            x="43.4171"
+            y="42.8531"
+            width="59"
+            height="59"
+            transform="rotate(-45 43.4171 42.8531)"
+            stroke="black"
+            strokeOpacity="0.12"
+          />
+        </svg>
+      </span>
+      <span className="fourth__logo">
+        <svg
+          width="200"
+          height="200"
+          viewBox="0 0 200 200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M99.5 64.6446C99.5 83.8946 83.8947 99.4999 64.6446 99.4999C45.3946 99.4999 29.7893 83.8946 29.7893 64.6446C29.7893 45.3945 45.3946 29.7892 64.6446 29.7892C83.8947 29.7892 99.5 45.3945 99.5 64.6446Z"
+            stroke="#AEB0B9"
+          />
+          <path
+            d="M170.211 64.6446C170.211 83.8946 154.605 99.4999 135.355 99.4999C116.105 99.4999 100.5 83.8946 100.5 64.6446C100.5 45.3945 116.105 29.7892 135.355 29.7892C154.605 29.7892 170.211 45.3945 170.211 64.6446Z"
+            stroke="#616681"
+          />
+          <path
+            d="M99.5 135.355C99.5 154.605 83.8947 170.211 64.6446 170.211C45.3946 170.211 29.7893 154.605 29.7893 135.355C29.7893 116.105 45.3946 100.5 64.6446 100.5C83.8947 100.5 99.5 116.105 99.5 135.355Z"
+            stroke="#58667E"
+          />
+          <path
+            d="M170.211 135.355C170.211 154.605 154.605 170.211 135.355 170.211C116.105 170.211 100.5 154.605 100.5 135.355C100.5 116.105 116.105 100.5 135.355 100.5C154.605 100.5 170.211 116.105 170.211 135.355Z"
+            stroke="#AEB0B9"
+          />
+          <ellipse
+            cx="100"
+            cy="50"
+            rx="35.3553"
+            ry="35.3553"
+            transform="rotate(45 100 50)"
+            stroke="black"
+            stroke-opacity="0.12"
+          />
+          <ellipse
+            cx="150"
+            cy="100"
+            rx="35.3553"
+            ry="35.3553"
+            transform="rotate(45 150 100)"
+            stroke="black"
+            stroke-opacity="0.12"
+          />
+          <ellipse
+            cx="50"
+            cy="100"
+            rx="35.3553"
+            ry="35.3553"
+            transform="rotate(45 50 100)"
+            stroke="black"
+            stroke-opacity="0.12"
+          />
+          <ellipse
+            cx="100"
+            cy="150"
+            rx="35.3553"
+            ry="35.3553"
+            transform="rotate(45 100 150)"
+            stroke="black"
+            stroke-opacity="0.12"
+          />
+        </svg>
+      </span>
+    </div>
+  );
+};
+
+
+export default LastColumn;
